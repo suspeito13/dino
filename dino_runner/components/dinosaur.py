1a29783b7fe72a2e17e738a42d36dee610b172a5
suspeito13 <141879068+suspeito13@users.noreply.github.com>
2023-08-14 21:36:31 -0300

    dino itallo

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,71 @@
+import pygame
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+
+
+X_POS = 80
+Y_POS = 310
+Y_DUCK_POS = Y_POS + 30
+JUMP_VEL = 8.5
+
+
+class Dinosaur:
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index = 0
+        self.jump_vel = JUMP_VEL
+        self.dino_run = True
+        self.dino_jump = False
+        self.dino_duck = False
+    
+    def update(self, user_input):
+        if self.dino_run:
+            self.run()
+        elif self.dino_jump:
+            self.jump()
+        elif self.dino_duck:
+            self.duck()
+
+        if user_input[pygame.K_UP] and not self.dino_jump:
+            self.dino_jump = True
+            self.dino_run = False
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = True
+            self.dino_duck = False
+    
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index += 1
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y = int(self.dino_rect.y - self.jump_vel * 4)
+            self.jump_vel -= 0.8
+
+        if self.jump_vel < -JUMP_VEL:
+            self.dino_rect.y = Y_POS
+            self.dino_jump = False
+            self.jump_vel = JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_DUCK_POS
+        self.step_index += 1
+        self.dino_duck = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,10 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
+FONT_STYLE = "freesansbold.ttf"
 
 class Game:
     def __init__(self):
@@ -12,16 +15,29 @@ class Game:
         self.clock = pygame.time.Clock()
         self.playing = False
         self.game_speed = 20
+        self.score = 0
+        self.death_count = 0
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
+
+    def execute(self):
+      self.runniug = True
+      while self.runniug:
+         if not self.show_menu()
+    pygame.display.quit()
+    pygame.quit()
+
 
     def run(self):
-        # Game loop: events - update - draw
         self.playing = True
+   # Game loop: events - update - draw
+       
         while self.playing:
-            self.events()
-            self.update()
-            self.draw()
+         self.events()
+         self.update()
+         self.draw()
         pygame.quit()
 
     def events(self):
@@ -29,13 +45,17 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
 
+    
     def update(self):
-        pass
-
+        user_input = pygame.key.get_pressed()                     
+        self.player.update(user_input)
+        self.obstacle_manager.update(self)
+    
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255)) 
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
