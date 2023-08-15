49bdb1baeec363d28f038cf2387537a795483db3
suspeito13 <141879068+suspeito13@users.noreply.github.com>
2023-08-15 01:24:36 -0300

    dina itallo 2

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,10 +1,10 @@
 import pygame
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 FONT_STYLE = "freesansbold.ttf"
+TEXT_COLOR_BLACK = (0, 0, 0)
 
 class Game:
     def __init__(self):
@@ -14,6 +14,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
+        self.running = False
         self.game_speed = 20
         self.score = 0
         self.death_count = 0
@@ -23,39 +24,46 @@ class Game:
         self.obstacle_manager = ObstacleManager()
 
     def execute(self):
-      self.runniug = True
-      while self.runniug:
-         if not self.show_menu()
-    pygame.display.quit()
-    pygame.quit()
-
+        self.running = True
+        while self.running:
+            if not self.playing:
+                self.show_menu()
+        pygame.display.quit()
+        pygame.quit()
 
     def run(self):
+        # Game loop: events - update - draw
         self.playing = True
-   # Game loop: events - update - draw
-       
+        self.obstacle_manager.reset_obstacles()
         while self.playing:
-         self.events()
-         self.update()
-         self.draw()
-        pygame.quit()
+            self.events()
+            self.update()
+            self.draw()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+                self.running = False
 
-    
     def update(self):
-        user_input = pygame.key.get_pressed()                     
+        user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
-    
+        self.update_score()
+
+    def update_score(self):
+        self.score += 1
+        if self.score % 100 == 0:
+            self.game_speed += 5
+
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255)) 
+        self.screen.fill((255, 255, 255))  # '#FFFFFF'
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
+        self.draw_score()
         pygame.display.update()
         pygame.display.flip()
 
@@ -67,3 +75,35 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def draw_score(self):
+        font = pygame.font.Font(FONT_STYLE, 22)
+        text = font.render(f"Score: {self.score}", True, TEXT_COLOR_BLACK)
+        text_rect = text.get_rect()
+        text_rect.center = (1000, 50)
+        self.screen.blit(text, text_rect)
+
+    def show_menu(self):
+        self.screen.fill((255, 255, 255))
+        half_screen_height = SCREEN_HEIGHT // 2
+        half_screen_width = SCREEN_WIDTH // 2
+
+        if self.death_count == 0:  # Tela de inicio
+            font = pygame.font.Font(FONT_STYLE, 22)
+            text = font.render("Press any key to start", True, TEXT_COLOR_BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rect)
+        else:  # Tela de restart
+            self.screen.blit(ICON, (half_screen_width - 20, half_screen_height - 140))
+            
+        pygame.display.update()
+        self.handle_events_on_menu()
+
+    def handle_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.run()
--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,10 +1,16 @@
 import random
 
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 from dino_runner.components.obstacles.obstacle import Obstacle
 
+CACTUS = [
+    (LARGE_CACTUS, 300),
+    (SMALL_CACTUS, 325),
+]
 
 class Cactus(Obstacle):
-    def __init__(self, image):
+    def __init__(self):
+        image, cactus_pos = CACTUS[random.randint(0, 1)]
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = cactus_pos
